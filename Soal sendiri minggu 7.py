ac3e42768215a86fc294d5a25e1476adc82b81e2
KevinTP300 <73930618+KevinTP300@users.noreply.github.com>
2021-03-22 20:38:58 +0700

    Create Soal sendiri minggu 7.py

---
+++ Soal sendiri minggu 7.py
@@ -0,0 +1,11 @@
+#Kevin Triantama Prayitno | 71180300
+#Universitas Kristen Duta Wacana
+
+masukan = "aku cinta indonesia"
+
+masuk = masukan.split(" ")
+cetak = masuk[0]+"_"+masuk[1]+"_"+masuk[2]
+cetakUp = masukan.upper()
+
+print(cetak)
+print(cetakUp)
