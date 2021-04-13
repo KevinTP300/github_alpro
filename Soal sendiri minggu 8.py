2788cb4883749d06b069d50a75356d21013bdea4
KevinTP300 <73930618+KevinTP300@users.noreply.github.com>
2021-04-13 21:52:59 +0700

    Create Soal sendiri minggu 8.py

---
+++ Soal sendiri minggu 8.py
@@ -0,0 +1,8 @@
+#Kevin Triantama Prayitno || 71180300
+#Universitas Kristen Duta Wacana
+
+hitungan = 5 * 2
+kalimat = " lima kali dua sama dengan sepuluh"
+
+print(hitungan, file = open ("test.txt", 'w'))
+print(kalimat, file = open("test.txt", 'a')) 
